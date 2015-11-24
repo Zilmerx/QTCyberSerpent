f8708e329851413fb9697e1957f954fd68c11ada
Zilmerx <hex404@gmail.com>
2015-11-24 12:12:09 -0500

    Shit qui marche.

--- QTCyberSerpent/Settings.h
+++ QTCyberSerpent/Settings.h
@@ -33,26 +33,28 @@ const QString QT_DEFAULT_NBOBSTACLE = "1";
 const QString QT_DEFAULT_CAMERANUM = "0";
 
 
-const std::chrono::milliseconds REFRESH_INTERVAL_MESSAGE = std::chrono::milliseconds(100);
+const std::chrono::milliseconds REFRESH_INTERVAL_MESSAGE = std::chrono::milliseconds(200);
 
 
 //////////////// GAMEPLAY ///////////////
 // Le nombre de points qui sont sur le terrain et peuvent être mangés par le robot.
 const int NB_POINTS_SIMULTANEE = 5;
 // Le nombre de blocs de la queue que l'on doit sauter, lors du calcul de collision.
-const int NB_QUEUE_NOCOLLISION = 4;
+const int NB_QUEUE_NOCOLLISION = 6;
 // Le nombre de blocs de la queue que l'on doit sauter avant d'imprimer le prochain.
 // Si NB_QUEUE_SAUTE = 5, cela veut dire qu'une image sur 5 sera affichée.
-const int NB_QUEUE_SAUTE = 5;
+const int NB_QUEUE_SAUTE = 10;
 // Le nombre de blocs de la queue que l'on doit sauter lors de l'impression (espace entre le bot et la queue).
-const int NB_QUEUEIMPRIM_SAUTE = 2;
+const int NB_QUEUEIMPRIM_SAUTE = 3;
 // Le nombre de detections hors zone qui doivent être fait avant de déclarer le joueur perdant.
 const int NB_HORSZONE_MAX = 5;
 
+const std::chrono::milliseconds REFRESH_INTERVAL_QUEUE = std::chrono::milliseconds(15);
+
+const cv::Vec3b COULEUR_CONE_MIN = cv::Vec3b{ 0, 115, 235 };
+const cv::Vec3b COULEUR_CONE_MAX = cv::Vec3b{ 0, 135, 255 };
 
 
 //////////////// COLLISION ///////////////
-const cv::Vec3b COULEUR_NONPRINT = cv::Vec3b{ 0, 255, 0 };
-
-
-
+const cv::Vec3b COULEUR_NONPRINT_MIN = cv::Vec3b{ 0, 0, 0 };
+const cv::Vec3b COULEUR_NONPRINT_MAX = cv::Vec3b{ 255, 255, 255 };
